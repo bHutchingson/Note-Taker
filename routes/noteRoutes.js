f2c437612d63fc7214464134f79ffc7dd0c97c44
Brandon <hutchingsonb@gmail.com>
2021-05-14 11:52:46 -0400

    added routes for getting index.html, and notes.html

--- routes/noteRoutes.js
+++ routes/noteRoutes.js
@@ -1,4 +1,17 @@
 const router = require('express').Router();
 const path = require('path');
-const store = require('../db/storingData');
+const router = express.Router();
+const fs = require('fs');
 
+//setting db.json to variable
+const db = require('../db/db.json');
+
+//get index.html
+router.get("/", (req, res) => {
+    res.sendFile(path.join(__dirname, "index.html"));
+});
+
+//get notes.html
+router.get("/notes", (req, res) => {
+    res.sendFile(path.join(__dirname, "notes.html"));
+});
